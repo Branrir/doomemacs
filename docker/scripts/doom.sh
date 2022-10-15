48c092aab7aa309967659f16fe7317d9c17e9117
Branrir <info@branrir.xyz>
2022-10-15 22:28:48 +0000

    chore(emacs): added linux/macos compatibility

--- docker/scripts/doom.sh
+++ docker/scripts/doom.sh
@@ -3,16 +3,6 @@
 # docker-desktop
 DOCKER_SOCKET=/var/run/docker.sock
 DOCKER_HOME=/home/doomguy
-VOLUMES="-v ${HOME}/.kube:${DOCKER_HOME}/.kube \
-         -v ${HOME}/.docker:${DOCKER_HOME}/.docker \
-         -v ${HOME}/.ssh:${DOCKER_HOME}/.ssh  \
-         -v ${HOME}/.gitconfig:${DOCKER_HOME}/.gitconfig \
-         -v ${HOME}/.config/glab-cli:${DOCKER_HOME}/.config/glab-cli \
-         -v ${HOME}/.config/argocd:${DOCKER_HOME}/.config/argocd \
-        -v ${HOME}:${DOCKER_HOME}/workdir \
-         -v ${DOCKER_SOCKET}.raw:${DOCKER_SOCKET} \
-         -v /run/host-services/ssh-auth.sock:/run/host-services/ssh-auth.sock \
-         -v ${HOME}/.zsh:${DOCKER_HOME}/.zsh_override"
 VARS="-e TERM=xterm-256color"
 
 # populate args if present
@@ -24,5 +14,29 @@ if [ $# -ne 0 ]; then
     fi
 fi
 
-# execute container
+# if macos
+if [[ $OSTYPE == 'darwin'* ]]; then
+    VOLUMES="-v ${HOME}/.kube:${DOCKER_HOME}/.kube \
+             -v ${HOME}/.docker:${DOCKER_HOME}/.docker \
+             -v ${HOME}/.ssh:${DOCKER_HOME}/.ssh  \
+             -v ${HOME}/.gitconfig:${DOCKER_HOME}/.gitconfig \
+             -v ${HOME}/.config/glab-cli:${DOCKER_HOME}/.config/glab-cli \
+             -v ${HOME}/.config/argocd:${DOCKER_HOME}/.config/argocd \
+             -v ${HOME}:${DOCKER_HOME}/workdir \
+             -v ${DOCKER_SOCKET}.raw:${DOCKER_SOCKET} \
+             -v /run/host-services/ssh-auth.sock:/run/host-services/ssh-auth.sock \
+             -v ${HOME}/.zsh:${DOCKER_HOME}/.zsh_override"
+else
+    VOLUMES="-v ${HOME}/.kube:${DOCKER_HOME}/.kube \
+             -v ${HOME}/.docker:${DOCKER_HOME}/.docker \
+             -v ${HOME}/.ssh:${DOCKER_HOME}/.ssh  \
+             -v ${HOME}/.gitconfig:${DOCKER_HOME}/.gitconfig \
+             -v ${HOME}/.config/glab-cli:${DOCKER_HOME}/.config/glab-cli \
+             -v ${HOME}/.config/argocd:${DOCKER_HOME}/.config/argocd \
+             -v ${HOME}:${DOCKER_HOME}/workdir \
+             -v ${DOCKER_SOCKET}:${DOCKER_SOCKET} \
+             -v ${SSH_AUTH_SOCK}:/run/host-services/ssh-auth.sock \
+             -v ${HOME}/.zsh:${DOCKER_HOME}/.zsh_override"
+fi    
+
 docker run -it -h emacs ${VARS} ${VOLUMES} branrir/doom-emacs ${ARG}
