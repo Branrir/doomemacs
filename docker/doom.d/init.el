180ad291b1f4d5325eea7d386eeb3c04637b1628
Branrir <info@branrir.xyz>
2024-03-10 04:50:48 +0100

    chore(doom): remove nerd-icons

--- docker/doom.d/init.el
+++ docker/doom.d/init.el
@@ -51,7 +51,6 @@
        window-select     ; visually switch windows
        workspaces        ; tab emulation, persistence & separate workspaces
        ;;zen               ; distraction-free coding or writing
-       nerd-icons
 
        :editor
        (evil +everywhere); come to the dark side, we have cookies
